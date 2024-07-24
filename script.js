e0c1e5ac995c97c10b572fa4cf4dbfb2edd8a898
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-07-23 23:56:16 -0600

    Add interactivity and functionality test.
    For each button in the calculator, a variable was
    added. There are now event listeners that change
    the calculator's buttons color on mouseover
    and mouseout.
    
    The equal sign was temporarily made to console log
    the firstNumber variable to test the new added
    functions: getFirstNumber, and getSecondNumber.
    
    Both functions make the event listeners on the
    number's buttons store the number in question
    in diferent arrays: firstNumber and secondNumber.

--- script.js
+++ script.js
@@ -1,4 +1,66 @@
+//Global Variables (might change later)
+
+let firstNumber = [];
+let secondNumber = [];
+let operator;
+
+//DOM node selection
+const num0 = document.querySelector("#cero");
+const num1 = document.querySelector("#one");
+const num2 = document.querySelector("#two");
+const num3 = document.querySelector("#three");
+const num4 = document.querySelector("#four");
+const num5 = document.querySelector("#five");
+const num6 = document.querySelector("#six");
+const num7 = document.querySelector("#seven");
+const num8 = document.querySelector("#eight");
+const num9 = document.querySelector("#nine");
+const sum = document.querySelector("#sum");
+const sub = document.querySelector("#sub");
+const mult = document.querySelector("#mult");
+const divi = document.querySelector("#divi");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+const buttons = document.querySelectorAll(".button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+
+
+//Event Listeners to change color on mouseover for all number buttons
+buttons.forEach((button) => button.addEventListener("mouseover", () => { button.style["background-color"] = "blue" }));
+buttons.forEach((button) => button.addEventListener("mouseout", () => { button.style["background-color"] = "rgb(57, 87, 87" }));
+
+//Event Listeners to change color on mouseover for all operator buttons
+operatorButtons.forEach((button) => button.addEventListener("mouseover", () => { button.style["background-color"] = "blue" }));
+operatorButtons.forEach((button) => button.addEventListener("mouseout", () => { button.style["background-color"] = "rgb(35, 60, 60)" }))
+
+//equals event listener, for now, just a test to print variables in the console
+equals.addEventListener("click", () => { console.log(firstNumber); });
+
+//Event listeners to add specified number to firstNumber array when clicking the number button
+num0.addEventListener("click", () => { getFirstNumber(0) });
+num1.addEventListener("click", () => { getFirstNumber(1) });
+num2.addEventListener("click", () => { getFirstNumber(2) });
+num3.addEventListener("click", () => { getFirstNumber(3) });
+num4.addEventListener("click", () => { getFirstNumber(4) });
+num5.addEventListener("click", () => { getFirstNumber(5) });
+num6.addEventListener("click", () => { getFirstNumber(6) });
+num7.addEventListener("click", () => { getFirstNumber(7) });
+num8.addEventListener("click", () => { getFirstNumber(8) });
+num9.addEventListener("click", () => { getFirstNumber(9) });
+
+
 //functions
+
+
+function getFirstNumber(number) {
+    firstNumber.push(number);
+}
+
+function getSecondNumber(number) {
+    secondNumber.push(number);
+}
+
+
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -16,9 +78,6 @@ function divide(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
 
 function operate(operator) {
     switch (operator) {
@@ -39,3 +98,4 @@ function operate(operator) {
             break;
     }
 }
+
